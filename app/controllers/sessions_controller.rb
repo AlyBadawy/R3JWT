b19d5ad69deca956bceae8612052cfa89f8b50ce
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-07-09 18:29:46 -0400

    Created login controller/method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    username = params["username"]
+    password = params["password"]
+    ip = request.ip
+    user_agent = request.user_agent
+
+    login_data = AuthHelper.login(username, password, ip, user_agent)
+
+    response.headers["access-token"] = login_data[:jwt]
+    response.headers["refresh-token"] = login_data[:refresh_token]
+    response.headers["expire-at"] = login_data[:exp]
   end
 
   def update
