d42998e27e12ca03655f8bf3dbc8c0e119047d31
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-07-09 17:42:32 -0400

    Created sessions controller and routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   scope "api" do
-    post "account", to: "account#create"
-    put "account", to: "account#update"
-    delete "account", to: "account#destroy"
+    post "account", to: "account#create" # Sign Up
+    put "account", to: "account#update" # update account info
+    delete "account", to: "account#destroy" # delete account
 
     resources :users
+    resources :sessions
   end
   root "react#index", as: :react_index
   get "*path", to: "react#index"
